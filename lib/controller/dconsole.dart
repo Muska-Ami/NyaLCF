8e7663111ea48f40f52c29e59ce64664c883e2bb
YanMo <xiajiaruimail@qq.com>
2023-12-29 18:25:30 +0800

    Update dconsole.dart

--- lib/controller/dconsole.dart
+++ lib/controller/dconsole.dart
@@ -2,45 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nyalcf/util/frpc/ProcessManager.dart';
 
-// 控制台控制器
+/// 控制台控制器
 class ConsoleController extends GetxController {
-  var widgets = <DataRow>[].obs; // UI组件列表
-  var process_list = <Map>[].obs; // 进程管理列表
+  var widgets = <DataRow>[].obs; /// UI组件列表
+  var process_list = <Map>[].obs; /// 进程管理列表
 
-  // 添加进程
+  /// 添加进程
   addProcess(p_map) {
     process_list.add(p_map);
     process_list.refresh();
   }
 
-  // 移除进程
+  /// 移除进程
   removeProcess(p_map) {
     process_list.remove(p_map);
     process_list.refresh();
   }
 
-  // 清空进程
+  /// 清空进程
   clearProcess() {
     process_list.clear();
     process_list.refresh();
   }
 
-  // 加载进程管理信息
+  /// 加载进程管理信息
   load() {
     widgets.value = <DataRow>[];
     process_list.forEach((element) {
       final process = element['process'];
       final proxy_id = element['proxy_id'];
 
-      // 添加进程管理信息至UI组件列表
+      /// 添加进程管理信息至UI组件列表
       widgets.add(DataRow(cells: [
-        DataCell(Text(process.pid.toString())), // 进程ID
-        DataCell(Text(proxy_id.toString())), // 代理ID
+        DataCell(Text(process.pid.toString())), /// 进程ID
+        DataCell(Text(proxy_id.toString())), /// 代理ID
         DataCell(
           IconButton(
             icon: Icon(Icons.close),
             onPressed: () {
-              // 杀死进程
+              /// 杀死进程
               FrpcProcessManager().kill(element);
             },
           ),
