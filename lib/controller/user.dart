05f15c8bc2374f49b720bdd0d4047c6a996996e0
YanMo <xiajiaruimail@qq.com>
2023-12-29 18:23:59 +0800

    Update user.dart

--- lib/controller/user.dart
+++ lib/controller/user.dart
@@ -4,20 +4,20 @@ import 'package:nyalcf/prefs/UserInfoPrefs.dart';
 
 // 用户控制器
 class UserController extends GetxController {
-  var user = ''.obs; // 用户名
-  var email = ''.obs; // 邮箱
-  var token = ''.obs; // 令牌
-  var avatar = 'https://cravatar.cn/avatar/'.obs; // 头像链接，使用CloudFlare Workers代理
-  var inbound = 0.obs; // 流入流量
-  var outbound = 0.obs; // 流出流量
-  var frp_token = ''.obs; // FRP令牌
-  var traffic = 0.obs; // 总流量
+  var user = ''.obs; /// 用户名
+  var email = ''.obs; /// 邮箱
+  var token = ''.obs; /// 令牌
+  var avatar = 'https://cravatar.cn/avatar/'.obs; /// 头像链接，爱来自cravatar（？）
+  var inbound = 0.obs; /// 流入流量
+  var outbound = 0.obs; /// 流出流量
+  var frp_token = ''.obs; /// FRP令牌
+  var traffic = 0.obs; /// 总流量
 
-  var welcomeText = '好'.obs; // 欢迎文字
+  var welcomeText = '好'.obs; /// 欢迎文字
 
-  // 加载方法
+  /// 加载方法
   load() async {
-    // 获取用户信息
+    /// 获取用户信息
     User userinfo = await UserInfoPrefs.getInfo();
     user.value = userinfo.user;
     email.value = userinfo.email;
@@ -30,7 +30,7 @@ class UserController extends GetxController {
 
     int hour = DateTime.now().hour;
 
-    // 根据小时确定欢迎文字
+    /// 根据小时确定欢迎文字
     if (hour <= 8) {
       welcomeText.value = '凌晨好';
     } else if (hour <= 12) {
