04952c3b4889080693bb5da01ce31bcf449f2322
ltzXiaoYanMo <xiajiaruimail@qq.com>
2024-01-10 20:05:30 +0800

    添加镜像源

--- lib/dio/basicConfig.dart
+++ lib/dio/basicConfig.dart
@@ -4,5 +4,6 @@ class basicConfig {
   static final frpc_config_url = 'https://www.locyanfrp.cn/api';
   static final github_api_url = 'https://api.github.com';
   static final github_main_url = 'https://github.com';
+  static final github_mirrors_url = 'https://mirror.ghproxy.com';
   // static final frpc_release_repo = 'LoCyan-Team/LoCyanFrpPureApp';
 }
