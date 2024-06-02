75fbbe2d62512f9625a7575e5adc7bf6725aba5e
夏沫花火zzz <xmcn@1l1.icu>
2024-06-02 17:15:13 +0800

    Fix state bug

--- nyalcf_core/lib/storages/injector.dart
+++ nyalcf_core/lib/storages/injector.dart
@@ -4,12 +4,12 @@ import 'package:nyalcf_core/storages/configurations/launcher_configuration_stora
 
 class StoragesInjector {
   static init() async {
-    final _lcs = LauncherConfigurationStorage();
-    final _fcs = FrpcConfigurationStorage();
+    final lcs = LauncherConfigurationStorage();
+    final fcs = FrpcConfigurationStorage();
     final aps = AutostartProxiesStorage();
 
-    await _lcs.initCfg();
-    await _fcs.initCfg();
+    await lcs.initCfg();
+    await fcs.initCfg();
     await aps.initCfg();
 
     /// Deprecated config load will remove in future.
