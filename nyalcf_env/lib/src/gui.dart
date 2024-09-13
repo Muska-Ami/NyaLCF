171a59ab56e78a05b9ae6c54f82ddc8daed588f9
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-13 14:52:02 +0800

    hmm?

--- nyalcf_env/lib/src/gui.dart
+++ nyalcf_env/lib/src/gui.dart
@@ -4,10 +4,10 @@ import 'package:dotenv/dotenv.dart';
 final _env = DotEnv(includePlatformEnvironment: true)..load();
 
 bool? ENV_GUI_DISABLE_AUTO_UPDATE_CHECK = bool.tryParse(
-  _env['NYA_LCF_DISABLE_AUTO_UPDATE_CHECK'] ?? '',
+  _env['NYA_LCF_GUI_DISABLE_AUTO_UPDATE_CHECK'] ?? '',
   caseSensitive: false,
 );
 bool? ENV_GUI_DISABLE_DEEPLINK = bool.tryParse(
-  _env['NYA_LCF_DISABLE_DEEPLINK'] ?? '',
+  _env['NYA_LCF_GUI_DISABLE_DEEPLINK'] ?? '',
   caseSensitive: false,
 );
