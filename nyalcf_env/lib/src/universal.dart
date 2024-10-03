b6e732b3747062a1ec1b2ec46809c240746f6cb3
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-10-04 02:25:12 +0800

    Do not out dotenv file not found logs

--- nyalcf_env/lib/src/universal.dart
+++ nyalcf_env/lib/src/universal.dart
@@ -1,7 +1,7 @@
 // Package imports:
 import 'package:dotenv/dotenv.dart';
 
-final _env = DotEnv(includePlatformEnvironment: true)..load();
+final _env = DotEnv(includePlatformEnvironment: true, quiet: true)..load();
 
 String? ENV_UNIVERSAL_FRPC_PATH = _env['NYA_LCF_FRPC_PATH'];
 String? ENV_UNIVERSAL_FRPC_DOWNLOAD_MIRROR_URL =
