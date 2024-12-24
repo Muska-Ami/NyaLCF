52c06dec0c2f7cd7ec311405ae3d76685c5b2b30
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-12-24 13:02:47 +0800

    Nothing changed

--- nyalcf_cli/lib/commands/authorize.dart
+++ nyalcf_cli/lib/commands/authorize.dart
@@ -4,20 +4,18 @@ import 'dart:io';
 
 // Package imports:
 import 'package:crypto/crypto.dart';
+// Project imports:
+import 'package:nyalcf/templates/command.dart';
 import 'package:nyalcf_core/models/user_info_model.dart';
 import 'package:nyalcf_core/network/client/api/auth/oauth/access_token.dart';
+import 'package:nyalcf_core/network/client/api/user/frp/token.dart'
+    as user_frp_token;
 import 'package:nyalcf_core/network/client/api/user/info.dart' as user_info;
 import 'package:nyalcf_core/network/client/api_client.dart';
 import 'package:nyalcf_core/network/server/oauth.dart';
 import 'package:nyalcf_core/storages/stores/user_info_storage.dart';
 import 'package:nyalcf_core/utils/logger.dart';
 import 'package:nyalcf_core_extend/storages/token_storage.dart';
-
-// Project imports:
-import 'package:nyalcf/templates/command.dart';
-
-import 'package:nyalcf_core/network/client/api/user/frp/token.dart'
-    as user_frp_token;
 
 class Authorize implements Command {
   static final _tokenStorage = TokenStorage();
@@ -30,6 +28,13 @@ class Authorize implements Command {
     final ApiClient api = ApiClient();
 
     await startHttpServer();
+    Logger.info(
+      'Please open this link to authorize: '
+      'http://localhost:5173/auth/oauth/authorize'
+      '?app_id=1'
+      '&scopes=User,Proxy,Sign'
+      '&redirect_url=http://localhost:21131/oauth/callback',
+    );
     Logger.write('Waiting callback...');
     while (!_callback) {}
     if (_refreshToken == null) exit(1);
