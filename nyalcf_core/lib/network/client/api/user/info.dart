08c8614073f4b350372d79fa5908f501d516a229
夏沫花火zzz <xmcn@1l1.icu>
2025-03-02 00:04:58 +0800

    Update info.dart

--- nyalcf_core/lib/network/client/api/user/info.dart
+++ nyalcf_core/lib/network/client/api/user/info.dart
@@ -4,10 +4,11 @@ import 'package:nyalcf_core/models/api_base_model.dart';
 class GetInfo extends ApiBaseModel {
   GetInfo({
     required num userId,
-  }) : super(endpoint: '/user/info', params: {
-          'user_id': userId,
-        }, validateStatus: [
-          200,
-          401
-        ]);
+  }) : super(
+    endpoint: '/user/info',
+    params: {
+      'user_id': userId,
+    },
+    validateStatus: [200, 401],
+  );
 }
